d05571a1aa91e3dc0b9514a5785b09e162e98125
Dima-Yu <mindwork.one@gmail.com>
2021-03-30 12:23:11 +0300

    edit temlate and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create 
@@ -14,7 +17,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your info was updated"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
